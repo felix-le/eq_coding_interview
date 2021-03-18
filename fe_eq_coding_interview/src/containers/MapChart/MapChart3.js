52d8a4ecf4521484892305a23d0b84b676c8886a
Viet Anh Le <mc.vietanh@gmail.com>
2021-03-18 12:18:22 -0400

    working on done map basic - working on additional detail

--- fe_eq_coding_interview/src/containers/MapChart/MapChart3.js
+++ fe_eq_coding_interview/src/containers/MapChart/MapChart3.js
@@ -7,7 +7,25 @@ const Marker = ({ children }) => children;
 const MapChart3 = ({ data }) => {
   const mapRef = useRef();
   const [zoom, setZoom] = useState(10);
-  const [bounds, setBounds] = useState(null);
+  console.log('🚀 ~ file: MapChart3.js ~ line 10 ~ MapChart3 ~ zoom', zoom);
+  const [bounds, setBounds] = useState({
+    ne: {
+      lat: 43.72994718039476,
+      lng: -78.9478252928626,
+    },
+    nw: {
+      lat: 43.72994718039476,
+      lng: -79.79960904530401,
+    },
+    se: {
+      lat: 43.55926889307838,
+      lng: -78.9478252928626,
+    },
+    sw: {
+      lat: 43.55926889307838,
+      lng: -79.79960904530401,
+    },
+  });
 
   const [dataMap, setDataMap] = useState([]);
 
@@ -21,7 +39,7 @@ const MapChart3 = ({ data }) => {
           ...item,
           type: 'Place',
           properties: {
-            category: item.type,
+            category: item.Type,
             cluster: false,
           },
           geometry: { type: 'Point', coordinates: [item.lon, item.lat] },
@@ -36,16 +54,21 @@ const MapChart3 = ({ data }) => {
   }, [data]);
 
   // get clusters
-  const { clusters } = useSupercluster({
+  const { clusters, supercluster } = useSupercluster({
     points: dataMap,
-    bounds,
-    zoom,
+    bounds: bounds,
+    zoom: zoom,
     options: { radius: 75, maxZoom: 20 },
   });
+
   console.log(
     '🚀 ~ file: MapChart3.js ~ line 40 ~ MapChart3 ~ clusters',
     clusters
   );
+  const _handlerZoomBounds = (zoom, bounds) => {
+    setZoom(zoom);
+    setBounds([bounds.nw.lng, bounds.se.lat, bounds.se.lng, bounds.nw.lat]);
+  };
 
   return (
     <div style={{ height: '100vh', width: '100%' }}>
@@ -57,32 +80,45 @@ const MapChart3 = ({ data }) => {
         onGoogleApiLoaded={({ map }) => {
           mapRef.current = map;
         }}
-        onChange={({ zoom, bounds }) => {
-          console.log(
-            '🚀 ~ file: MapChart3.js ~ line 73 ~ MapChart3 ~  zoom, bounds',
-            zoom,
-            bounds
-          );
-
-          // if (zoom !== undefined && bounds !== undefined) {
-          // }
-          // setZoom(zoom);
-          // setBounds([
-          //   bounds.nw.lng,
-          //   bounds.se.lat,
-          //   bounds.set.lng,
-          //   bounds.nw.lat,
-          // ]);
-        }}
+        onChange={({ zoom, bounds }) => _handlerZoomBounds(zoom, bounds)}
       >
-        {data.length > 0 &&
-          data.map((p) => (
-            <Marker key={p.poi_id} lat={p.lat} lng={p.lon}>
+        {clusters.map((cluster) => {
+          const [longtitude, latitude] = cluster.geometry.coordinates;
+          const {
+            cluster: isCluster,
+            point_count: point_count,
+          } = cluster.properties;
+
+          if (isCluster) {
+            return (
+              <Marker key={cluster.id} lat={latitude} lng={longtitude}>
+                <div
+                  className='cluster-marker'
+                  style={{
+                    width: `${20 + (point_count / dataMap.length) * 40}px`,
+                    height: `${20 + (point_count / dataMap.length) * 40}px`,
+                  }}
+                  onClick={() => {
+                    const expansionZoom = Math.min(
+                      supercluster.getClusterExpansionZoom(cluster.id)
+                    );
+                    mapRef.current.setZoom(expansionZoom);
+                    mapRef.current.panTo({ lat: latitude, lng: longtitude });
+                  }}
+                >
+                  {point_count}
+                </div>
+              </Marker>
+            );
+          }
+          return (
+            <Marker key={cluster.poi_id} lat={latitude} lng={longtitude}>
               <div className='icon-wrapper'>
                 <FaBuilding />
               </div>
             </Marker>
-          ))}
+          );
+        })}
       </GoogleMapReact>
     </div>
   );
